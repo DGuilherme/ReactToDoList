185ddf13ec4a839f1109a7f2749e14cfc517eae7
David Guilherme <47979584+DGuilherme@users.noreply.github.com>
2022-03-05 22:45:00 +0000

    generated project

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # ReactToDoList
+
+
+
+# source https://www.youtube.com/watch?v=E1E08i2UJGI&ab_channel=BrianDesign
---
+++ react-todoapp-v1/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="ToDo-App">
+      <h1>Todo List</h1>  
+    </div>
+  );
+}
+
+export default App;
